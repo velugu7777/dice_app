4f60ac199bb0ee491f463cc0c1816e741c424a30
ramanajaneyulu velugu <rama@ramanajaneyulus-MacBook-Pro.local>
2021-09-22 23:02:37 +0530

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,57 @@ class DiceApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text('Dice App'),
+          centerTitle: true,
+          backgroundColor: Colors.red,
         ),
+        backgroundColor: Colors.red,
+        body: DicePage(),
+      ),
+    );
+  }
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                child: Image(
+                  image: AssetImage('images/dice1.png'),
+                ),
+                onPressed: () {},
+                // style: ButtonStyle(
+                //   backgroundColor: MaterialStateProperty.all(Colors.red),
+                // ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                child: Image(
+                  image: AssetImage('images/dice1.png'),
+                ),
+                onPressed: () {},
+                // style: ButtonStyle(
+                //   backgroundColor: MaterialStateProperty.all(Colors.red),
+                // ),
+              ),
+            ),
+          ),
+
+          // Image(image: AssetImage('images/dice1.png')),
+        ],
       ),
     );
   }
